2051dd1a70dc84a99181fbc032d140776a840634
unknown <marcomilad111@gmail.com>
2026-04-21 02:34:53 +0200

    Refactor: separate UI and worker threads and add progress tracking

--- downloader.py
+++ downloader.py
@@ -2,32 +2,35 @@ import yt_dlp
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QObject
 from PyQt5.QtWidgets import QProgressBar
 
-ydl_opts = {'format': 'bestvideo+bestaudio/best',
-            'ffmpeg_location': 'C:/ffmpeg/bin',
-            #'quiet': True,
-            #'no_warnings': True,
-            'noplaylist': False,
-            'outtmpl': 'C:/Users/LENOVO/Desktop/%(title)s.%(ext)s',
-            'merge_output_format': 'mp4'}
-
 
 
 class downloader(QObject):
+    progressSignal = pyqtSignal(int)
+    statusSignal = pyqtSignal(str)
     def __init__(self):
         super().__init__()
-        self.progressSignal = pyqtSignal(int)
-        self.finishedSignal = pyqtSignal()
-        self.errorSignal = pyqtSignal()
-
-    def set_url(self, url):
-        self.url = url
-
+        self.url = None
 
     def yt_progress(self, progress_dictionary):
         if progress_dictionary['status'] == 'downloading':
+            percentage = progress_dictionary.get('_percent_str', '0%').replace('%', '')
+            percentage = int(float(percentage))
+            self.progressSignal.emit(percentage)
+            status = f"{progress_dictionary['_speed_str']} - {progress_dictionary['_eta_str']} remaining"
+            self.statusSignal.emit(status)
 
 
 
-    def download(self, url):
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+
+    def download(self):
+        ydl_opts = {'format': 'bestvideo+bestaudio/best',
+                    'ffmpeg_location': 'C:/ffmpeg/bin',
+                    # 'quiet': True,
+                    # 'no_warnings': True,
+                    'noplaylist': False,
+                    'outtmpl': 'C:/Users/LENOVO/Desktop/%(title)s.%(ext)s',
+                    'merge_output_format': 'mp4',
+                    "progress_hooks": [self.yt_progress], }
+        if self.url:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([self.url])
--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, \
+    QPushButton, QProgressBar, QMessageBox
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QObject
 from downloader import downloader
 
@@ -15,12 +16,25 @@ class MainWindow(QMainWindow):
 
         self.initUI()
 
-        #working with the url
-        url = self.textbox1.text()
 
-        #working with the backend
+        self.download_manager.progressSignal.connect(self.progressBar.setValue)
+        self.download_manager.statusSignal.connect(self.statusLabel.setText)
+
+
+        #tell the thread what to do when it starts
         self.background_thread.started.connect(self.download_manager.download)
 
+        #working with the backend
+        self.download_button.clicked.connect(self.start_download)
+
+    def start_download(self):
+        url = self.textbox1.text()
+        self.download_manager.url = url
+        if not url:
+            QMessageBox.critical(self, "Error", "Please enter a valid URL")
+        else:
+            self.download_button.setEnabled(False)
+            self.background_thread.start()
 
     def initUI(self):
         #defining the UI's characteristics
@@ -36,10 +50,14 @@ class MainWindow(QMainWindow):
 
         #Defining everything
         self.layout1 = QVBoxLayout()
+        self.layout2 = QVBoxLayout()
         self.main_layout.addLayout(self.layout1)
+        self.main_layout.addLayout(self.layout2)
         self.download_button = QPushButton("Download")
         self.label1 = QLabel("Enter Video Link:")
         self.textbox1 = QLineEdit()
+        self.progressBar = QProgressBar()
+        self.statusLabel = QLabel()
 
         #Adding stuff to layout 1
         self.layout1.addWidget(self.label1)
@@ -47,9 +65,9 @@ class MainWindow(QMainWindow):
         self.layout1.addWidget(self.download_button)
         self.layout1.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
 
-        #working with the backend
-        self.download_button.clicked.connect(self.background_thread.start)
-
+        # Adding stuff to layout 2
+        self.layout2.addWidget(self.progressBar)
+        self.layout2.addWidget(self.statusLabel)
 
         #SETTING THE CENTRAL WIDGET
         self.setCentralWidget(self.main_widget)
